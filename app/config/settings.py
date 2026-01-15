94b35ad486e536548c92e4f974598b74be09d46a
Tejashree-Khot <tejashreekhot.777@gmail.com>
2026-01-15 20:19:35 +0900

    feat: update env variables

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,30 +5,22 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
-    SERVICE_HOST: str | None = Field(default="localhost")
-    SERVICE_PORT: int | None = Field(default=8080)
-    DEV: bool = Field(default=True)
-
     CHUNK_SIZE: int = Field(default=1000)
     CHUNK_OVERLAP: int = Field(default=200)
-    BATCH_SIZE: int = Field(default=50)
 
-    # for better accuracy, use intfloat/multilingual-e5-base, 768, for faster, use all-MiniLM-L6-v2, 384, intfloat/e5-base-instruct
-    EMBEDDING_PROVIDER: str = Field(default="huggingface")
-    EMBEDDING_MODEL_NAME: str = Field(default="all-MiniLM-L6-v2")
-    EMBEDDING_DIM: int = Field(default=384)
-
-    # Hugging Face
+    # Embedding Settings
     HF_TOKEN: SecretStr = SecretStr("hf_api_key")
+    EMBEDDING_MODEL_NAME: str = Field(default="all-MiniLM-L6-v2")
+
+    # LLM Settings
+    GROQ_API_KEY: SecretStr = SecretStr("groq_api_key")
+    LLM_MODEL_NAME: str = Field(default="meta-llama/llama-4-maverick-17b-128e-instruct")
 
     # Milvus Settings
     MILVUS_URI: str = Field(default="http://localhost:19530")
     MILVUS_TOKEN: str = Field(default="")
-    MILVUS_COLLECTION_NAME: str = Field(default="thesis_rag")
-
-    # LLM Settings
-    LLM_MODEL_NAME: str = Field(default="meta-llama/llama-4-maverick-17b-128e-instruct")
-    GROQ_API_KEY: SecretStr = SecretStr("groq_api_key")
+    MILVUS_COLLECTION_NAME: str = Field(default="eval_rag")
+    MILVUS_EMBEDDING_DIM: int = Field(default=384)
 
     # Postgress Settings
     POSTGRES_USER: str | None = Field(default="postgres")
